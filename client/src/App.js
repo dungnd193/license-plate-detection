5486f3fe9a7f3782367edfe685d7024af3fdb155
dungnd193 <dungnd.work@gmail.com>
2023-06-26 16:38:49 +0700

    fix UI

--- client/src/App.js
+++ client/src/App.js
@@ -79,58 +79,62 @@ function App() {
       <div className="license-plate-recognition">
         <h1>License Plate Recognition</h1>
         <div className="plate-container">
-          <div className="plate-type-container">
-            <label>
-              <input
-                type="radio"
-                name="plateType"
-                value={ONE_LINE}
-                onChange={handlePlateTypeChange}
-                checked={plateType === ONE_LINE}
-              />
-              One line Plate
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="plateType"
-                value={TWO_LINE}
-                onChange={handlePlateTypeChange}
-                checked={plateType === TWO_LINE}
-              />
-              Two line Plate
-            </label>
+          <div>
+            {previewImage ? <img src={imgPath ? `http://127.0.0.1:5000/image/${imgPath}` : previewImage} alt="Preview" className="preview-plate" /> : <div className="placeholder">
+              <p>No picture selected. Please select one picture to recognize!</p>
+            </div>}
           </div>
-          {previewImage ? <img src={imgPath ? `http://127.0.0.1:5000/image/${imgPath}` : previewImage} alt="Preview" className="preview-plate" /> : <div className="placeholder">
-            <p>No picture selected. Please select one picture to recognize!</p>
-          </div>}
-          {showLoading && <div className="lds-ring"><div></div><div></div><div></div><div></div></div>}
-          {plates && plates.length > 0 && plates.map((plate, idx) => (
-            plate && <div className="plate-number-group" key={idx}>
-              <div className="result-group">
-                <h2>Biển số xe:</h2>
-                <p>{plate.plate_text.replace(/[\[\]\(\)\|{}]/g, "")}</p>
-              </div>
-              <div className="result-group">
-                <h2>Object detection confidence:</h2>
-                <p>{plate.object_detection_confidence < 1 ? (plate.object_detection_confidence * 100).toFixed(5) : plate.object_detection_confidence}%</p>
-              </div>
-              <div className="result-group">
-                <h2>OCR confidence:</h2>
-                <p>{plate.ocr_confidence < 1 ? (plate.ocr_confidence * 100).toFixed(5) : plate.ocr_confidence}%</p>
-              </div>
+          <div>
+            <div className="plate-type-container">
+              <label>
+                <input
+                  type="radio"
+                  name="plateType"
+                  value={ONE_LINE}
+                  onChange={handlePlateTypeChange}
+                  checked={plateType === ONE_LINE}
+                />
+                One line Plate
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  name="plateType"
+                  value={TWO_LINE}
+                  onChange={handlePlateTypeChange}
+                  checked={plateType === TWO_LINE}
+                />
+                Two line Plate
+              </label>
             </div>
-          ))}
-          {showNotRecognize && <div className="placeholder">
-            <p><strong>Can not recognize any plate in picture!</strong></p>
-          </div>}
-          <br />
+            <div className="button-group">
+              <input type="file" ref={fileInputRef} hidden onChange={handleFileChange} />
+              <button className="choose-file-btn" onClick={() => { fileInputRef.current.click() }}>Choose file</button>
+              <button onClick={handleUpload}>Recognize Plate</button>
+            </div>
+            {showLoading && <div className="lds-ring"><div></div><div></div><div></div><div></div></div>}
+            {plates && plates.length > 0 && plates.map((plate, idx) => (
+              plate && <div className="plate-number-group" key={idx}>
+                <div className="result-group">
+                  <h2>Biển số xe:</h2>
+                  <p>{plate.plate_text.replace(/[\[\]\(\)\|{}]/g, "")}</p>
+                </div>
+                <div className="result-group">
+                  <h2>Object detection confidence:</h2>
+                  <p>{plate.object_detection_confidence < 1 ? (plate.object_detection_confidence * 100).toFixed(5) : plate.object_detection_confidence}%</p>
+                </div>
+                <div className="result-group">
+                  <h2>OCR confidence:</h2>
+                  <p>{plate.ocr_confidence < 1 ? (plate.ocr_confidence * 100).toFixed(5) : plate.ocr_confidence}%</p>
+                </div>
+              </div>
+            ))}
+            {showNotRecognize && <div className="placeholder">
+              <p><strong>Can not recognize any plate in picture!</strong></p>
+            </div>}
+          </div>
         </div>
-        <div className="button-group">
-          <input type="file" ref={fileInputRef} hidden onChange={handleFileChange} />
-          <button className="choose-file-btn" onClick={() => { fileInputRef.current.click() }}>Choose file</button>
-          <button onClick={handleUpload}>Recognize Plate</button>
-        </div>
+
       </div>
     </div >
   );
